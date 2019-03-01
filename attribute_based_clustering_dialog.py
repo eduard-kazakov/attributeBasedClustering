0b104c9b76fd98dbfe372e7c9c176aa553dc2712
Eduard Kazakov <ee.kazakov@gmail.com>
2019-03-01 14:37:43 +0300

    ver. 2.1. K-Means2 added. Much more scipy algorithms options added.

--- attribute_based_clustering_dialog.py
+++ attribute_based_clustering_dialog.py
@@ -36,16 +36,20 @@ class AttributeBasedClusteringDialog(QtWidgets.QDialog, FORM_CLASS):
         self.setupUi(self)
 
         # UI Preparation
-        self.clusteringThresholdLine.setDisabled(True)
-        self.linkageMethodComboBox.setDisabled(True)
+        #self.clusteringThresholdLine.setDisabled(True)
+        #self.linkageMethodComboBox.setDisabled(True)
         self.vectorLayerComboBox.setFilters(QgsMapLayerProxyModel.VectorLayer)
         self.fieldsComboBox.setLayer(self.vectorLayerComboBox.currentLayer())
         self.fieldsComboBox.setFilters(QgsFieldProxyModel.Numeric)
+        self.maxNumberOfClustersSpinBox.setDisabled(True)
         self.progressBar.setVisible(False)
 
         # Buttons
         self.vectorLayerComboBox.currentIndexChanged.connect(self.layerChanged)
+        self.tabWidget.currentChanged.connect(self.tabChanged)
         self.clusteringMethodComboBox.currentIndexChanged.connect(self.methodChanged)
+
+        self.criterionComBox.currentIndexChanged.connect(self.criterionChanged)
 
         self.addFieldButton.clicked.connect(self.addField)
         self.deleteSelectedButton.clicked.connect(self.deleteSelectedFields)
@@ -58,19 +62,58 @@ class AttributeBasedClusteringDialog(QtWidgets.QDialog, FORM_CLASS):
         self.fieldsComboBox.setLayer(self.vectorLayerComboBox.currentLayer())
         self.fieldsComboBox.setFilters(QgsFieldProxyModel.Numeric)
 
+    def tabChanged(self):
+        if self.tabWidget.currentIndex() == 3:
+            self.clusteringMethodComboBox.setCurrentIndex(3)
+
+        if self.tabWidget.currentIndex() == 2:
+            self.clusteringMethodComboBox.setCurrentIndex(2)
+
+        if self.tabWidget.currentIndex() == 1:
+            self.clusteringMethodComboBox.setCurrentIndex(1)
+
+        if self.tabWidget.currentIndex() == 0:
+            self.clusteringMethodComboBox.setCurrentIndex(0)
+
+
     def methodChanged(self):
-        if self.clusteringMethodComboBox.currentIndex() == 0:
-            self.clusteringThresholdLine.setDisabled(True)
-            self.linkageMethodComboBox.setDisabled(True)
-            self.numberOfClustersSpinBox.setEnabled(True)
-        if self.clusteringMethodComboBox.currentIndex() == 1:
-            self.clusteringThresholdLine.setDisabled(True)
-            self.linkageMethodComboBox.setDisabled(True)
-            self.numberOfClustersSpinBox.setEnabled(True)
-        if self.clusteringMethodComboBox.currentIndex() == 2:
-            self.clusteringThresholdLine.setEnabled(True)
-            self.linkageMethodComboBox.setEnabled(True)
+        if self.clusteringMethodComboBox.currentIndex() == 3:
+            self.tabWidget.setCurrentIndex(3)
             self.numberOfClustersSpinBox.setDisabled(True)
+
+
+        if self.clusteringMethodComboBox.currentIndex() == 0:
+            self.tabWidget.setCurrentIndex(0)
+            self.numberOfClustersSpinBox.setEnabled(True)
+            #self.clusteringThresholdLine.setDisabled(True)
+            #self.linkageMethodComboBox.setDisabled(True)
+            #self.numberOfClustersSpinBox.setEnabled(True)
+
+        if self.clusteringMethodComboBox.currentIndex() == 1:
+            self.tabWidget.setCurrentIndex(1)
+            self.numberOfClustersSpinBox.setEnabled(True)
+            #self.clusteringThresholdLine.setDisabled(True)
+            #self.linkageMethodComboBox.setDisabled(True)
+            #self.numberOfClustersSpinBox.setEnabled(True)
+        if self.clusteringMethodComboBox.currentIndex() == 2:
+            self.tabWidget.setCurrentIndex(2)
+            self.numberOfClustersSpinBox.setEnabled(True)
+            #self.clusteringThresholdLine.setEnabled(True)
+            #self.linkageMethodComboBox.setEnabled(True)
+            #self.numberOfClustersSpinBox.setDisabled(True)
+
+    def criterionChanged(self):
+        if self.criterionComBox.currentIndex() == 0:
+            self.maxNumberOfClustersSpinBox.setDisabled(True)
+            self.clusteringThresholdLine.setEnabled(True)
+
+        if self.criterionComBox.currentIndex() == 1:
+            self.maxNumberOfClustersSpinBox.setDisabled(True)
+            self.clusteringThresholdLine.setEnabled(True)
+
+        if self.criterionComBox.currentIndex() == 2:
+            self.maxNumberOfClustersSpinBox.setEnabled(True)
+            self.clusteringThresholdLine.setDisabled(True)
 
 
     def checkIfTableIsCorrect(self):
@@ -139,20 +182,62 @@ class AttributeBasedClusteringDialog(QtWidgets.QDialog, FORM_CLASS):
             QtWidgets.QMessageBox.critical(None, "Error", 'No output field name specified!')
             return
 
-        if self.clusteringMethodComboBox.currentIndex() == 2:
+
+        # Inputs validation
+
+        if self.clusteringMethodComboBox.currentIndex() == 0:
             try:
-                clustering_threshold = float(self.clusteringThresholdLine.text())
+                kmeans_clustering_threshold = float(self.kmeansThresholdLineEdit.text())
             except Exception as e:
                 QtWidgets.QMessageBox.critical(None, "Error", 'Threshold must be float! %s' % str(e))
+                return
+
+            try:
+                kmeans_iter = int(self.kmeansIterSpinBox.text())
+            except Exception as e:
+                QtWidgets.QMessageBox.critical(None, "Error", 'Number of iterations must be integer! %s' % str(e))
+                return
+
+        if self.clusteringMethodComboBox.currentIndex() == 1:
+
+            try:
+                kmeans2_iter = int(self.kmeans2IterSpinBox.text())
+            except Exception as e:
+                QtWidgets.QMessageBox.critical(None, "Error", 'Number of iterations must be integer! %s' % str(e))
+                return
+
+        if self.clusteringMethodComboBox.currentIndex() == 3:
+            try:
+                hier2_threshold = float(self.clusteringThresholdLine.text())
+            except Exception as e:
+                QtWidgets.QMessageBox.critical(None, "Error", 'Threshold must be float! %s' % str(e))
+                return
+
+            try:
+                hier2_max_clusters = int(self.maxNumberOfClustersSpinBox.text())
+            except Exception as e:
+                QtWidgets.QMessageBox.critical(None, "Error", 'Max number of clusters must be integer! %s' % str(e))
+                return
+
+            try:
+                hier2_depth = int(self.depthSpinBox.text())
+            except Exception as e:
+                QtWidgets.QMessageBox.critical(None, "Error", 'Depth must be integer! %s' % str(e))
                 return
 
         vectorLayer = self.vectorLayerComboBox.currentLayer()
         normalize = self.checkBox.isChecked()
         clusterNumber = int(self.numberOfClustersSpinBox.text())
-        linkageMethod = self.linkageMethodComboBox.currentText()
         outputFieldName = self.outputFieldNameLine.text()
 
+        hier2_linkageMethod = self.linkageMethodComboBox.currentText()
+        hier2_criterion = self.criterionComBox.currentText()
+        hier2_metric = self.metricComboBox.currentText()
+
+        kmeans2_method = self.kmeans2MethodComboBox.currentText()
+
         attributesList = []
+
         allRows = self.fieldsTable.rowCount()
         for row in range(0, allRows):
             attributesList.append([self.fieldsTable.item(row, 0).text(), float(self.fieldsTable.item(row, 1).text())])
@@ -163,7 +248,7 @@ class AttributeBasedClusteringDialog(QtWidgets.QDialog, FORM_CLASS):
            self.deactivateInterface()
            QApplication.processEvents()
            try:
-               self.kmeansClustering(vectorLayer, attributesList, normalize, clusterNumber, outputFieldName)
+               self.kmeansClustering(vectorLayer, attributesList, normalize, clusterNumber, kmeans_iter, kmeans_clustering_threshold, outputFieldName)
            except Exception as e:
                self.activateInterface()
                QtWidgets.QMessageBox.critical(None, "Error", 'Problems during clustering... %s' % str(e))
@@ -172,7 +257,23 @@ class AttributeBasedClusteringDialog(QtWidgets.QDialog, FORM_CLASS):
            self.activateInterface()
            QtWidgets.QMessageBox.about(None, "Success", "Done!")
 
+
         if self.clusteringMethodComboBox.currentIndex() == 1:
+            self.deactivateInterface()
+            QApplication.processEvents()
+            try:
+                self.kmeans2Clustering(vectorLayer, attributesList, normalize, clusterNumber, kmeans2_iter, kmeans2_method,
+                                               outputFieldName)
+            except Exception as e:
+                self.activateInterface()
+                QtWidgets.QMessageBox.critical(None, "Error", 'Problems during clustering... %s' % str(e))
+                return
+
+            self.activateInterface()
+            QtWidgets.QMessageBox.about(None, "Success", "Done!")
+
+
+        if self.clusteringMethodComboBox.currentIndex() == 2:
             self.deactivateInterface()
             QApplication.processEvents()
             try:
@@ -186,12 +287,14 @@ class AttributeBasedClusteringDialog(QtWidgets.QDialog, FORM_CLASS):
             self.activateInterface()
             QtWidgets.QMessageBox.about(None, "Success", "Done!")
 
-        if self.clusteringMethodComboBox.currentIndex() == 2:
+        if self.clusteringMethodComboBox.currentIndex() == 3:
             self.deactivateInterface()
             QApplication.processEvents()
             try:
-                self.hierarchicalClusteringScipy(vectorLayer, attributesList, normalize, clustering_threshold, linkageMethod,
-                                               outputFieldName)
+                self.hierarchicalClusteringScipy(vectorLayer, attributesList, normalize, hier2_threshold,
+                                                 hier2_linkageMethod, hier2_criterion, hier2_metric,
+                                                 hier2_depth, hier2_max_clusters, outputFieldName)
+
             except Exception as e:
                 self.activateInterface()
                 QtWidgets.QMessageBox.critical(None, "Error", 'Problems during clustering... %s' % str(e))
@@ -346,7 +449,7 @@ class AttributeBasedClusteringDialog(QtWidgets.QDialog, FORM_CLASS):
         vectorLayer.commitChanges()
 
     # Performs K-Means clustering. Updates vector layer field.
-    def kmeansClustering(self, vectorLayer, attributesList, normalize, clusterNumber, outputFieldName):
+    def kmeansClustering(self, vectorLayer, attributesList, normalize, clusterNumber, iterations, threshold, outputFieldName):
         from scipy.cluster.vq import kmeans, vq
         from numpy import array
 
@@ -379,7 +482,7 @@ class AttributeBasedClusteringDialog(QtWidgets.QDialog, FORM_CLASS):
 
         data = array(fullObjectsList)
 
-        centroids, _ = kmeans(data, clusterNumber, 25)
+        centroids, _ = kmeans(data, clusterNumber, iter=iterations, thresh=threshold)
         idx, _ = vq(data, centroids)
         idx = idx.tolist()
 
@@ -402,8 +505,72 @@ class AttributeBasedClusteringDialog(QtWidgets.QDialog, FORM_CLASS):
         vectorLayer.updateFields()
         vectorLayer.commitChanges()
 
+        # Performs K-Means clustering. Updates vector layer field.
+    def kmeans2Clustering(self, vectorLayer, attributesList, normalize, clusterNumber, iterations, method,
+                         outputFieldName):
+
+        from scipy.cluster.vq import kmeans2
+        from numpy import array
+        import numpy as np
+
+        fullObjectsList = []
+        features = vectorLayer.getFeatures()
+
+        for feature in features:
+            fullObjectsList.append([])
+            for attribute in attributesList:
+                if feature[attribute[0]]:
+                    fullObjectsList[len(fullObjectsList) - 1].append(feature[attribute[0]])
+                else:
+                    fullObjectsList[len(fullObjectsList) - 1].append(0)
+
+        # NORMALIZING
+        if normalize:
+            i = 0
+            maxValues = []
+            while i < len(attributesList):
+                maxValues.append(max(abs(item[i]) for item in fullObjectsList))
+                i += 1
+
+            j = 0
+            while j < len(fullObjectsList):
+                i = 0
+                while i < len(fullObjectsList[j]):
+                    fullObjectsList[j][i] = (fullObjectsList[j][i] * 1.0) / (maxValues[i] * 1.0)
+                    i += 1
+                j += 1
+
+        data = array(fullObjectsList)
+
+        try:
+            centroids, labels = kmeans2(data, clusterNumber, iter=iterations, minit=method)
+        except np.linalg.LinAlgError as err:
+            centroids, labels = kmeans2(data, clusterNumber, iter=iterations, minit='points')
+
+        idx = labels.tolist()
+
+
+        vectorLayerDataProvider = vectorLayer.dataProvider()
+
+        # Create field of not exist
+        if vectorLayer.fields().indexFromName(outputFieldName) == -1:
+            vectorLayerDataProvider.addAttributes([QgsField(outputFieldName, QVariant.Int)])
+
+        vectorLayer.updateFields()
+        vectorLayer.startEditing()
+        attrIdx = vectorLayer.fields().indexFromName(outputFieldName)
+        features = vectorLayer.getFeatures()
+
+        i = 0
+        for feature in features:
+            vectorLayer.changeAttributeValue(feature.id(), attrIdx, int(idx[i]))
+            i += 1
+
+        vectorLayer.updateFields()
+        vectorLayer.commitChanges()
+
     # Performs Hierarchical clustering from scipy with unknown number of clusters. Updates vector layer field.
-    def hierarchicalClusteringScipy(self, vectorLayer, attributesList, normalize, clusterThreshold, linkageMethod, outputFieldName):
+    def hierarchicalClusteringScipy(self, vectorLayer, attributesList, normalize, clusterThreshold, linkageMethod, criterion, metric, depth, max_clust, outputFieldName):
         import scipy.cluster.hierarchy as hcluster
         from numpy import array
 
@@ -436,7 +603,12 @@ class AttributeBasedClusteringDialog(QtWidgets.QDialog, FORM_CLASS):
 
         data = array(fullObjectsList)
 
-        clusters = hcluster.fclusterdata(data, clusterThreshold, criterion="distance", method=linkageMethod)
+        if criterion == 'maxclust':
+            clusters = hcluster.fclusterdata(data, t=max_clust, criterion=criterion, method=linkageMethod,
+                                             metric=metric, depth=depth)
+        else:
+            clusters = hcluster.fclusterdata(data, t=clusterThreshold, criterion=criterion, method=linkageMethod,
+                                             metric=metric, depth=depth)
 
         vectorLayerDataProvider = vectorLayer.dataProvider()
 #
